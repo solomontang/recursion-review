eae5ae268c455bdd33464c1a7fff6b84add2930f
solomontang <solomoncztang@gmail.com>
2017-05-15 12:13:04 -0700

    Complete getElementByClassName problem

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,29 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  // get document body
+  var body = document.body;
+  // result array
+  var results = [];
+  // call test function 
+  var getNodes = function(element) {
+    //check if the classList conatains class name
+    if (element.classList.contains(className)) {
+      // push the result to empty array
+      results.push(element);
+    }
+    if (element.childNodes) {
+      element.childNodes.forEach(function(childElement){
+        //check if childNodes contains className
+        console.log(childElement);
+        if (childElement.classList) {
+          // use recursion
+          getNodes(childElement);
+        }
+      });
+    } 
+  };
+  getNodes(body);
+  return results;
 };
+
